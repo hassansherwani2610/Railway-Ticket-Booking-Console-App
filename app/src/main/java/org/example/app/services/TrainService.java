dd3cd72985fd0764f1e38f7e178943e2991f29d9
hassansherwani10 <hassansherwani633@gmail.com>
2026-01-16 23:15:46 +0500

    seventh commit

--- app/src/main/java/org/example/app/services/TrainService.java
+++ app/src/main/java/org/example/app/services/TrainService.java
@@ -7,6 +7,7 @@ import org.example.app.entities.Train;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class TrainService {
@@ -43,5 +44,25 @@ public class TrainService {
     // This tells us that the train is valid to go the desired source and destination
     public List<Train> searchTrains(String source , String destination){
         return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    // To add a New Train in our Data in our file (Local DB)
+    public void addTrain(Train newTrain){
+        Optional<Train> trainExists = trainList.stream().filter(train -> train.getTrainId().equalsIgnoreCase(newTrain.getTrainId())).findFirst();
+
+        if (trainExists.isPresent()){
+            // If a train with the same "trainId" exists, update it instead of adding a new one
+            updateTrain(newTrain);
+        }
+        else {
+            // Otherwise, add the new train to the list
+            trainList.add(newTrain);
+            saveTrainListToFile();
+        }
+    }
+
+    // To update the Data of already existing Train
+    public void updateTrain(Train updatedTrain){
+
     }
 }
